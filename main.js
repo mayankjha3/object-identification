0052a3164e6dc0682ea3dc6a8dca5f0b20431835
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2022-01-03 15:33:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,26 +4,34 @@ myStatus = "";
 objects = [];
 
 function preload(){
-    img = loadImage("dog_cat.jpg");
 }
 
 function setup(){
-    canvas = createCanvas(640,420);
+    canvas = createCanvas(380,380);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
 
     objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
 }
 
 function draw(){
-    image(img , 0 , 0 , 640 , 420);
+    image(video , 0 , 0 , 380 , 380);
 
     if(myStatus != ""){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video , gotResult);
         for(i = 0; i < objects.length; i++){
             console.log("stactus:object detected");
-
-            fill("#ff0000");
-            stroke("#ff0000");
+            document.getElementById("status").innerHTML = "Status : Detecting Objects";
+            document.getElementById("number_of_objects").innerHTML = "number of objects :" + objects.length;
+            fill(r , g , b);
+            stroke(r , g , b);
             noFill();
             percent = floor(objects[i].confidence*100);
             text( objects[i].label + " " + percent + " %", objects[i].x , objects[i].y);
@@ -36,7 +44,7 @@ function modelLoaded(){
     console.log("model has loaded");
 
     myStatus = true;
-    objectDetector.detect(img , gotResult);
+    objectDetector.detect(video , gotResult);
 }
 
 function gotResult(error , results){
